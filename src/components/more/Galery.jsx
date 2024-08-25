eed99da915ff1f6c838cc71f7890002c22691658
José de Jesús Zárate García <jose.jesus.zarate.garcia@outlook.com>
2024-08-25 16:18:20 -0600

    Galery added

--- src/components/more/Galery.jsx
+++ src/components/more/Galery.jsx
@@ -5,10 +5,15 @@ const Galery = ({galeryImg}) => {
   return (
     <div className='mt-7 border-l-2 border-[#00AFFF] w-[90%] mb-5'>
             <h1 className='text-2xl text-[#00AFFF] font-bold pl-2'>GALERÍA</h1>
-            <div className='flex flex-wrap gap-10 mt-5'>
+            <div className='flex flex-wrap gap-10 mt-5 ml-2'>
                 {galeryImg &&
                     galeryImg.map((imagen, id) => (<div key={id} className='object-cover w-52'>
-                        <Image src={"/galeria/"+galeryImg} height={1000} width={1000}/>
+                        <Image src={"/galeria/"+imagen.img} height={1000} width={1000} className='rounded-xl'/>
+                        <p className='text-center font-bold'>
+                          <span className='text-[#00AFFF]'>{"<"}</span>
+                          <span className='text-white'>{imagen.desc}</span>
+                          <span className='text-[#00AFFF]'>{"/>"}</span>
+                        </p>
                     </div>)
                 )}
             </div>
